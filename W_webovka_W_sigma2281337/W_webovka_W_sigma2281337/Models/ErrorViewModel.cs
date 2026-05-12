ade5918540c0c22cc11ca33ea905213a6549bb92
shtrudellll <egor.chensov@gmail.com>
2026-05-12 15:56:39 +0200

    aaaaaaaaaa

---
+++ W_webovka_W_sigma2281337/W_webovka_W_sigma2281337/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace W_webovka_W_sigma2281337.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
