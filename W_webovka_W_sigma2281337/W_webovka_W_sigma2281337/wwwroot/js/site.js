02699f47ab541eb499b20f56252fe42c474b4b66
Ivan66lit <popov.1507i@gmail.com>
2026-05-24 13:46:50 +0200

    upravy designu

--- W_webovka_W_sigma2281337/W_webovka_W_sigma2281337/wwwroot/js/site.js
+++ W_webovka_W_sigma2281337/W_webovka_W_sigma2281337/wwwroot/js/site.js
@@ -2,3 +2,50 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+new TradingView.widget({
+    "container_id": "tradingview_bitcoin",
+    "width": "100%",
+    "height": 400,
+    "symbol": "BINANCE:BTCUSDT",
+    "interval": "D",
+    "theme": "light",
+    "style": "1",
+    "locale": "en",
+});
+
+new TradingView.widget({
+    "container_id": "apple",
+    "width": "100%",
+    "height": 300,
+    "symbol": "NASDAQ:AAPL",
+    "interval": "D",
+    "theme": "light"
+});
+
+new TradingView.widget({
+    "container_id": "tesla",
+    "width": "100%",
+    "height": 300,
+    "symbol": "NASDAQ:TSLA",
+    "interval": "D",
+    "theme": "light"
+});
+
+new TradingView.widget({
+    "container_id": "microsoft",
+    "width": "100%",
+    "height": 300,
+    "symbol": "NASDAQ:MSFT",
+    "interval": "D",
+    "theme": "light"
+});
+
+new TradingView.widget({
+    "container_id": "gold",
+    "width": "100%",
+    "height": 300,
+    "symbol": "OANDA:XAUUSD",
+    "interval": "D",
+    "theme": "light"
+});
